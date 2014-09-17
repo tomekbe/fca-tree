b0253ea4a297ce182ec63a03a07a9d5a750dc6a7
Tomasz Bartkowiak <pixelfury@gmail.com>
2014-09-17 12:14:38 +0100

    base href

---
+++ app/dec-tree.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    $("#fca-app-wrap").append("<div id='fca-app'>This message shows until this div has not been bootstraped x</div>");	  
+
+    
+    $("head").append("<script src='bower_components/angular/angular.js'></script>");
+  	$("head").append("<script src='bower_components/angular-resource/angular-resource.js'></script>");
+  	$("head").append("<script src='bower_components/angular-cookies/angular-cookies.js'></script>");
+    $("head").append("<script src='bower_components/angular-animate/angular-animate.js'></script>");
+
+    $("head").append("<script src='bower_components/angular-sanitize/angular-sanitize.js'></script>");
+    $("head").append("<script src='bower_components/angular-route/angular-route.js'></script>");
+
+    $("body").append("<script src='scripts/app.js'></script>");
+    $("body").append("<script src='scripts/services.js'></script>");
+    $("body").append("<script src='scripts/controllers/main.js'></script>");
+
+
+    $("head").append('<link rel="stylesheet" type="text/css" href="styles/main.css">');   
+    $("head").append('<link rel="stylesheet" type="text/css" href="styles/animate.css">'); 
+
+})
